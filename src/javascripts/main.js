afd2baa8efff5f64d1606bcab242d59062b87243
Josh Thomas <joshuathomas2@mail.weber.edu>
2021-02-23 21:23:37 -0700

    config files

---
+++ src/javascripts/main.js
@@ -0,0 +1,16 @@
+// Required by Webpack - do not touch
+require.context('../', true, /\.(html|json|txt|dat)$/i)
+require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
+require.context('../stylesheets/', true, /\.(css|scss)$/i)
+
+// TODO
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function Main (props) {
+    return (
+        <div>Hello world!</div>
+    )
+}
+
+ReactDOM.render(<Main/>, document.getElementById('main'))
