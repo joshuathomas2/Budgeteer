38db4b1a8f4b73f2b4adb258c30e18dbb93a12ef
Josh Thomas <joshuathomas2@mail.weber.edu>
2021-03-12 15:55:30 -0700

    added debug <p> tags in html, uncomment require in main.js

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,5 +1,5 @@
 // Required by Webpack - do not touch
-//require.context('../', true, /\.(html|json|txt|dat)$/i)
+require.context('../', true, /\.(html|json|txt|dat)$/i)
 require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
 require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
