55ac01a8c745079fe4fd376c28d407deadf6d295
jhtens22 <jaythanhew@mail.weber.edu>
2021-04-11 17:48:17 -0600

    Categories

---
+++ src/javascripts/components/CategoriesList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export function CategoriesList(props) {
+    return(
+        <>
+            <section id="categories" class="bg-light my-5">
+                <div class="row">
+                    <div class="col-6">
+                        <h3 class="text-secondary my-3">Category -</h3>
+                        <img></img>
+                        <p class="text-secondary">Planned: </p><br></br>
+                        <p class="text-secondary">Received: </p><br></br>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
