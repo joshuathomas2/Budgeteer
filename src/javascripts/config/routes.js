9e86e0b2aeb37e0febcc1279909281268f93e718
koltennay52 <60151320+koltennay52@users.noreply.github.com>
2021-04-10 23:58:29 -0600

    category controller completed

--- src/javascripts/config/routes.js
+++ src/javascripts/config/routes.js
@@ -3,8 +3,8 @@ import jwt from 'jsonwebtoken';
 import {homePage, registerPage, loginPage, categoriesListPage, categoryPage, transactionsListPage, transactionForm} from '../controllers/index';
 import { loginUserAPI, registerUserAPI } from '../controllers/user';
 import {allTransactionsAPI, oneTransactionAPI, createTransactionAPI, updateTransactionAPI, deleteTransactionAPI} from '../controllers/transaction';
-// import {allCategoriesAPI, oneCategoryAPI, createCategoriesAPI, updateCategoryAPI, deleteCategoryAPI} from '../controllers/category';
-import {allLabelsAPI, oneLabelAPI, createLabelAPI, updateLabelAPI, deleteLabelAPI} from '../controllers/label';
+import {allCategoriesAPI, oneCategoryAPI, createCategoriesAPI, updateCategoryAPI, deleteCategoryAPI} from '../controllers/category';
+// import {allLabelsAPI, oneLabelAPI, createLabelAPI, updateLabelAPI, deleteLabelAPI} from '../controllers/label';
 
 
 import { APP_SECRET } from './vars';
@@ -46,18 +46,18 @@ export function configureRoutes(app) {
     router.delete('/api/v1/transactions/:transactionID', deleteTransactionAPI);
 
     //CATEGORIES API
-    // router.get('/api/categories', allCategoriesAPI)
-    // router.get('/api/categories/:id', oneCategoryAPI)
-    // router.post('/api/categories', createCategoriesAPI)
-    // router.put('/api/categories/:id', updateCategoryAPI)
-    // router.delete('/api/categories/:id', deleteCategoryAPI)
+    router.get('/api/v1/categories', allCategoriesAPI)
+    router.get('/api/v1/categories/:categoryID', oneCategoryAPI)
+    router.post('/api/v1/categories', createCategoriesAPI)
+    router.put('/api/v1/categories/:categoryID', updateCategoryAPI)
+    router.delete('/api/v1/categories/:categoryID', deleteCategoryAPI)
 
     //LABELS API
-    router.get('/api/labels', allLabelsAPI);
-    router.get('/api/labels/:labelID', oneLabelAPI);
-    router.post('/api/labels', createLabelAPI);
-    router.put('/api/labels/:labelID', updateLabelAPI);
-    router.delete('/api/labels/:labelID', deleteLabelAPI);
+    // router.get('/api/labels', allLabelsAPI);
+    // router.get('/api/labels/:labelID', oneLabelAPI);
+    // router.post('/api/labels', createLabelAPI);
+    // router.put('/api/labels/:labelID', updateLabelAPI);
+    // router.delete('/api/labels/:labelID', deleteLabelAPI);
 
 
     // USERS API
---
+++ src/javascripts/controllers/category.js
@@ -0,0 +1,81 @@
+import {Category} from '../models/Category'
+
+export const allCategoriesAPI = (req, res, next) => {
+    Category.find().exec((err, categories) => {
+        if (err) {
+            res.json({success: false, message: 'allCategoriesAPI failed', err});
+            res.end();
+        } else {
+            res.json({categories, success: true, message: 'allCategoriesAPI passed'});
+            res.end();
+        }
+    })
+}
+
+export const oneCategoryAPI = (req, res, next) => {
+    const categoryID = req.params.categoryID;
+
+    Category.find({_id: categoryID}).exec((err, category) => {
+        if (err) {
+            res.json({success: false, message: 'oneCategoryAPI failed', err});
+            res.end();
+        } else {
+            res.json({category, success: true, message: 'oneCategoryAPI passed'});
+            res.end();
+        }
+    })
+}
+
+export const createCategoriesAPI = (req, res, next) => {
+    let category = new Category();
+
+    category.user_id = req.body.user_id;
+    category.name = req.body.name;
+    category.created_date = new Date;
+    category.modified_date = new Date;
+
+    category.save(err => {
+        if (err) {
+            res.json({success: false, message: 'createCategoryAPI failed', err});
+            res.end();
+        } else {
+            res.json({success: true, message: 'createCategoryAPI passed'});
+            res.end();
+        }
+    })
+}
+
+export const updateCategoryAPI = (req, res, next) => {
+    const categoryID = req.params.categoryID;
+
+    Category.findOne({_id: categoryID}).exec((err, category) => {
+        Category.updateOne({_id: categoryID}, {
+            user_id: req.body.user_id ? req.body.user_id : category.user_id,
+            name: req.body.name ? req.body.name : category.name,
+            modified_date: new Date()
+        }, err => {
+            if (err) {
+                console.log(err);
+                res.json({success: false, message: 'updateCategoryAPI failed', err});
+                res.end();
+            } else {
+                res.json({success: true, message: 'updateCateogryAPI passed'})
+                res.end();
+            }
+        })
+    })
+}
+
+export const deleteCategoryAPI = (req, res, next) => {
+    const categoryID = req.params.categoryID;
+
+    Category.deleteOne({_id: categoryID}, err => {
+        if (err) {
+            res.json({success: false, message: 'deleteCategoryAPI failed', err});
+            res.end();
+        } else {
+            res.json({success: true, message: 'deleteCategoryAPI passed'})
+            res.end();
+        }
+    })
+}
