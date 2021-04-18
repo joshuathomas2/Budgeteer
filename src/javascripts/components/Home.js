cd2fd9a3b0a16a42b8074c7db710ca15c650cb09
koltennay52 <60151320+koltennay52@users.noreply.github.com>
2021-04-18 15:50:02 -0600

    added dynamic list of categories for user on home page

--- src/javascripts/components/Home.js
+++ src/javascripts/components/Home.js
@@ -1,6 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {HomeCategoryListItem} from './HomeCategoryListItem'
+import { useCookies } from 'react-cookie'
 
 export function Home(props) {
+	const [categories, setCategories] = useState()
+  const [cookies, setCookie, removeCookie] = useCookies(['token'])
+  const [userID, setUserID] = useState()
+
+  useEffect(() => {
+    if (!userID) {
+      fetch('/api/v1/users/getCurrentUser', {
+        credentials: "same-origin"
+      })
+      .then(response => response.text())
+      .then(data => {
+        const retrieved_id = JSON.parse(data);
+        setUserID(retrieved_id);
+        //console.log(retrieved_id);
+      })
+    }
+  })
+
+  useEffect(() => {
+    if(userID) {
+      if(!categories) {
+        fetch(`/api/v1/categories/user/${userID}`, {
+          credentials: "same-origin"
+        })
+         .then(response => response.text())
+         .then(data => {
+           const retrieved_categories = JSON.parse(data);
+           setCategories(retrieved_categories);
+           //console.log('Categories: ' + retrieved_categories);
+         })
+      }
+    } 
+  })
+
+  if (!categories){
+    return (<span className="text-center">Loading data...</span>)
+  } else {
 	return (
 		<>
 			<header className="jumbotron my-4 bg-light">
@@ -21,11 +60,11 @@ export function Home(props) {
 					<div className="col-3 mx-3 mb-5">
 						<a href="#"><h3 className="text-center text-secondary mb-5">Categories</h3></a>
 						<div className="list-group text-center">
-							<a href="#" className="list-group-item list-group-item-action b-border bg-light">Cras justo odio</a>
-							<a href="#" className="list-group-item list-group-item-action b-border bg-light">Dapibus ac facilisis in</a>
-							<a href="#" className="list-group-item list-group-item-action b-border bg-light">Morbi leo risus</a>
-							<a href="#" className="list-group-item list-group-item-action b-border bg-light">Porta ac consectetur ac</a>
-							<a href="#" className="list-group-item list-group-item-action b-border bg-light">Vestibulum at eros</a>
+						{
+     					 	categories.map(c => {
+        					return <HomeCategoryListItem key={c._id} category={c}/>
+      						})
+    					}
 						</div>
 					</div>
 					<div className="col-7 mr-5 mb-5">
@@ -42,4 +81,5 @@ export function Home(props) {
 			</section>
 		</>
 	)
+					}
 }
